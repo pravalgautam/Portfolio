45fa18d046f7e4cf3704b6510d1280e572d66a7a
Anuj Singh <anujpratapsinghchbs@gmail.com>
2024-12-18 15:26:43 +0530

    changes in home by Anuj

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,16 +8,16 @@ const Home = () => {
           Praval Gautam
         </h1>
       
-        <div className="flex items-center ml-3">
+        {/* <div className="flex items-center ml-3">
         <div className="h-8 w-8 rounded-full bg-green-600 animate-ping flex justify-center items-center">
           <div className="h-4 w-4 rounded-full bg-green-400"></div>
         </div>
-        <h2 className="text-green-400 text-lg sm:text-xl p-4">
+        <h2 className=" hidden text-green-400 text-lg sm:text-xl p-4">
         Intern at Xeta Analytics
         </h2>
        
       </div>
-      <h3 className='text-green-400 text-lg sm:text-xl p-4'>Looking for a job</h3>
+      <h3 className='hidden text-green-400 text-lg sm:text-xl p-4'>Looking for a job</h3> */}
 </div>
   )
 }
